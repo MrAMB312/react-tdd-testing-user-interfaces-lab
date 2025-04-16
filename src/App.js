5e666c6a5230ffdff3e638afcf6828b7d5e01acf
Alex Blomarz <a.blomarz@gmail.com>
2025-04-15 21:30:17 -0500

    complete react-tdd-testing-user-interfaces-lab

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
 function App() {
-  return <div>Your code here</div>;
+  return (
+    <div>
+      <h1>Hi, I'm Shrek</h1>
+      <img 
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRGBJNn17c7LgybhYhnRzeeH4IyZx5lQQ69Mg&s"
+        alt="picture of me"
+      />
+      <h2>About Me</h2>
+      <p>I live in a swamp and I have a donkey</p>
+      <a href="https://github.com/MrAMB312">GitHub</a>
+      <a href="https://www.linkedin.com/in/alex-blomarz-2a489b26b">LinkedIn</a>
+    </div>
+  );
 }
 
 export default App;
--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -4,3 +4,58 @@ import '@testing-library/jest-dom';
 import App from "../App";
 
 // Your tests here
+test("displays a top-level heading with the text `Hi, I'm Shrek`", () => {
+    // Arrange
+    render(<App />);
+
+    // Act
+    const topLevelHeading = screen.getByRole("heading", {
+        name: /hi, i'm/i,
+        exact: false,
+        level: 1,
+    });
+
+    // Assert
+    expect(topLevelHeading).toBeInTheDocument();
+});
+test("displays an image of yourself with alt text identifying the content of the image", () => {
+    render(<App />);
+
+    const image = screen.getByAltText("picture of me");
+
+    expect(image).toBeInTheDocument();
+
+});
+test("displays a second-level heading with the text `About Me`", () => {
+    render(<App />);
+
+    const secondLevelHeading = screen.getByRole("heading", {
+        name: /About Me/,
+        exact: true,
+        level: 2,
+    })
+    
+    expect(secondLevelHeading).toBeInTheDocument;
+});
+test("displays a paragraph for your biography", () => {
+    render(<App />);
+
+    const paragraph = screen.getByText(/i live in a swamp/i);
+
+    expect(paragraph).toBeInTheDocument();
+});
+test("displays two links, one to your GitHub page, and one to your LinkedIn page", () => {
+    render(<App />);
+
+    const gitHubLink = screen.getByRole("link", {
+        name: /GitHub/i,
+        exact: false,
+    });
+    const linkedInLink = screen.getByRole("link", {
+        name: /LinkedIn/i,
+        exact: false,
+    });
+
+    expect(gitHubLink).toBeInTheDocument();
+    expect(linkedInLink).toBeInTheDocument();
+})
